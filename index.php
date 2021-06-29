a30865e47f5a72de24925057886e378435bda640
Chethan Bhat <chethanbhat@gmail.com>
2021-06-29 20:14:47 +0530

    Fix Minor Typo in URL

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $the_query = new WP_Query( array(
                     <?php
                         $types = get_the_terms( get_the_ID(), 'pokemon-type' );
                         foreach ($types as $type) {
-                            echo '<a class="page_link" href="' . esc_url(site_url('/')) . 'pokemon-types/'  . $type->slug  . '"><h5 class="pokedex_type ' . $type->name . '">' . $type->name . '</h5></a>';
+                            echo '<a class="page_link" href="' . esc_url(site_url('/')) . 'pokemon-type/'  . $type->slug  . '"><h5 class="pokedex_type ' . $type->name . '">' . $type->name . '</h5></a>';
                         }
                     ?>
                 </div>
